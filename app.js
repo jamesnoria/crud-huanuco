246cda7cf9dac09114afc729087a0454cda1969e
cristian16260 <124100432+cristian16260@users.noreply.github.com>
2023-03-19 18:01:05 -0500

    update method created

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import mysql from 'mysql2';
-import express from 'express';
+import mysql from "mysql2";
+import express from "express";
 
 const app = express();
 const port = 3000;
@@ -7,26 +7,22 @@ const DATABASE_URL =
   'mysql://gdq3xenn5sxmipaf22u8:pscale_pw_iBcBCcOHOb6j3JnYutqOfxBeFBtuC8OL5GPTLui8xrC@us-east.connect.psdb.cloud/crud-huanuco?ssl={"rejectUnauthorized":true}';
 
 const connection = mysql.createConnection(DATABASE_URL);
-console.log('Conectado a la base de datos');
+console.log("Conectado a la base de datos");
 
 app.use(express.json());
 
-
 // ESTE ENDPOINT ES PARA VER LOS USUARIOS O PERSONAS
-app.get('/personas', (req, res) => {
-  connection.query(
-    'SELECT * FROM `Persons`',
-    function (err, results, fields) {
-      res.json({
-        message: 'Lista de personas',
-        datos: results,
-      });
-    }
-  );
+app.get("/personas", (req, res) => {
+  connection.query("SELECT * FROM `Persons`", function (err, results, fields) {
+    res.json({
+      message: "Lista de personas",
+      datos: results,
+    });
+  });
 });
 
 // ESTE ENDPOINT ES PARA CREAR UNA PERSONA
-app.post('/create', (req, res) => {
+app.post("/create", (req, res) => {
   const PersonId = req.body.PersonId;
   const LastName = req.body.LastName;
   const FirstName = req.body.FirstName;
@@ -34,11 +30,11 @@ app.post('/create', (req, res) => {
   const City = req.body.City;
 
   connection.query(
-    'INSERT INTO `crud-huanuco`.`Persons` (PersonId, LastName, FirstName, Address, City) VALUES (?, ?, ?, ?, ?)',
+    "INSERT INTO `crud-huanuco`.`Persons` (PersonId, LastName, FirstName, Address, City) VALUES (?, ?, ?, ?, ?)",
     [PersonId, LastName, FirstName, Address, City],
     function (err, results) {
       res.json({
-        message: 'Persona creada',
+        message: "Persona creada",
         datos: results,
       });
     }
@@ -46,17 +42,35 @@ app.post('/create', (req, res) => {
 });
 
 // ESTE ENDPOINT ES PARA BORRAR UNA PERSONA
-app.delete('/borrarpersona', (req, res) => {
+app.delete("/borrarpersona", (req, res) => {
   const PersonId = req.body.PersonId;
 
   connection.query(
-    'DELETE FROM `crud-huanuco`.`Persons` WHERE PersonId = ?',
+    "DELETE FROM `crud-huanuco`.`Persons` WHERE PersonId = ?",
     [PersonId],
     function (err, results) {
       res.json({
-        message: 'Persona borrada',
+        message: "Persona borrada",
         datos: results,
       });
+    }
+  );
+});
+
+app.put("/actualizarpersona", (req, res) => {
+  const PersonId = req.body.PersonId;
+  const LastName = req.body.LastName;
+  const FirstName = req.body.FirstName;
+  const Address = req.body.Address;
+  const City = req.body.City;
+  connection.query(
+    "UPDATE `crud-huanuco`.`Persons` SET LastName = ?, FirstName = ?, Address = ?, City = ? WHERE PersonId = ?",
+    [LastName, FirstName, Address, City, PersonId],
+    function(err,results){
+      res.json({
+        message: `La perosna con el id=${PersonId} fue actualizada`,
+        datos: results,
+      })
     }
   );
 });
