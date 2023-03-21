47f122da7e48049ad6b83e7b495d521545c2c311
cristian16260 <124100432+cristian16260@users.noreply.github.com>
2023-03-21 17:39:07 -0500

    update in post

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import mysql from "mysql2";
-import express from "express";
+import express, { json } from "express";
 import camelcase from "camelcase";
 
 const app = express();
@@ -29,7 +29,7 @@ app.post("/create", (req, res) => {
   const FirstName = req.body.FirstName;
   const Address = req.body.Address;
   const City = req.body.City;
-  
+
   const Lastname = LastName.toLowerCase()
   const Firstname = FirstName.toLowerCase()
   const city = City.toLowerCase()
@@ -37,8 +37,9 @@ app.post("/create", (req, res) => {
   const minuscula = Address.toLowerCase()
   const address = minuscula[0].toUpperCase() + minuscula.substring(1)
 
+  connection.query("ALTER TABLE `Persons` ADD UNIQUE (PersonID)")
   connection.query(
-    "INSERT INTO `crud-huanuco`.`Persons` (PersonID, LastName, FirstName, Address, City) VALUES (?, ?, ?, ?, ?)",
+    "INSERT INTO `crud-huanuco`.`Persons` (PersonID , LastName, FirstName, Address, City) VALUES (? , ?, ?, ?, ?)",
     [PersonID, camelcase(Lastname,{pascalCase:true}), camelcase(Firstname,{pascalCase:true}), address, camelcase(city,{pascalCase:true})],
     function (err, results) {
       res.json({
@@ -46,12 +47,15 @@ app.post("/create", (req, res) => {
         datos: results,
       });
     }
-  );
+  ); 
 });
 
 // ESTE ENDPOINT ES PARA BORRAR UNA PERSONA
 app.delete("/borrarpersona", (req, res) => {
   const PersonID = req.body.PersonID;
+  // const LastName = req.body.LastName;
+  // const FirstName = req.body.FirstName;
+  // const City = req.body.City;
 
   connection.query(
     "DELETE FROM `crud-huanuco`.`Persons` WHERE PersonID = ?",
